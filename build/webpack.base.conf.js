f2c8ff5025e26f49599b9d3f165ca24953ab7377
xiaogang <1321044565@qq.com>
2017-10-17 16:33:54 +0800

    AMS使用vue+router搭建

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -4,6 +4,8 @@ const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
 
+const webpack=require('webpack');
+
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
 }
@@ -64,4 +66,5 @@ module.exports = {
       }
     ]
   }
+
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,84 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+import MainPage from '@/components/MainPage'
+import Monitor from '@/components/Monitor'
+import HongSearchIndex from '@/components/HongSearchIndex'
+import HongSearch from '@/components/HongSearch'
+import Recommend from '@/components/Recommend'
+import Analyze from '@/components/Analyze'
+
+import ListMode from '@/components/mode/ListMode'
+import TableMode from '@/components/mode/TableMode'
+
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: HelloWorld
-    }
+  	{	path: '/mainPage',name: 'MainPage',component: MainPage },
+    {	
+    	path: '/monitor',
+    	name: 'Monitor',
+    	component: Monitor,
+    	children:[ 
+    		{ path:"/",component:ListMode },
+            { path:"listMode",component:ListMode },
+            { path:"tableMode",component:TableMode },
+            { path: '*',redirect: '/ListMode'}
+        ]
+    },
+    {	path: '/hsindex',
+    	name: 'HongSearchIndex',
+    	component: HongSearchIndex,
+    },
+    {	path: '/hongsearch',
+    	name: 'HongSearch',
+    	component: HongSearch,
+    	children:[ 
+    		{ path:"/",component:ListMode },
+            { path:"listMode",component:ListMode },
+            { path:"tableMode",component:TableMode },
+            { path: '*',redirect: '/ListMode'}
+        ]
+    },
+    {	path: '/recommend',
+    	name: 'recommend',
+    	component: Recommend,
+    	children:[ 
+    		{ path:"/",component:ListMode },
+            { path:"listMode",component:ListMode },
+            { path:"tableMode",component:TableMode },
+            { path: '*',redirect: '/ListMode'}
+        ]
+    },
+    {	path: '/analyze',name: 'Analyze',component: Analyze },
+    {	path: '/',redirect: '/mainPage' },
+    {	path: '*',redirect: '/mainPage' }
   ]
 })
+
+/*
+export default [{
+	path: '/home',
+	component: Home
+}, {
+	path: '/follow',
+	component: Follow
+}, {
+	path: '/column',
+	component: Column
+}, {
+	path: '/user-info',
+	component: UserInfo
+}, {
+	path: '/article/:id',
+	component: Article
+}, {
+	path: '/',
+	redirect: '/home'
+}, {
+	path: '*',
+	redirect: '/home'
+}]
+*/
